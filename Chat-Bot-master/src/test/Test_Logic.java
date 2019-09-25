ab0fd1a16b8daf36f71587747b73441fd75d839e
LexLippi <43090493+LexLippi@users.noreply.github.com>
2019-09-25 13:09:52 +0500

    add package java

--- Chat-Bot-master/src/test/Test_Logic.java
+++ Chat-Bot-master/src/test/Test_Logic.java
@@ -1,6 +1,7 @@
 package test;
-import main.GameLogic;
-import main.Data;
+import main.java.Data;
+import main.java.GameLogic;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -11,26 +12,26 @@ class Test_Logic {
 	@Test
 	void testNoCity() {
 		var logic = new GameLogic();
-		Assert.assertEquals("Я не знаю такого города. Попробуй еще раз", logic.answer("арарарарар", new Data()));
+		Assert.assertEquals("пїЅ пїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ. пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅ", logic.answer("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", new Data()));
 	}
 	
 	@Test
 	void testRightCity() {
 		var logic = new GameLogic();
-		Assert.assertEquals("Агидель", logic.answer("Елабуга", new Data()));
+		Assert.assertEquals("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ", logic.answer("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ", new Data()));
 	}
 	
 	@Test
 	void testCityOnWrongLetter() {
 		var data = new Data();
 		var logic = new GameLogic();
-		logic.answer("Ханой", data);
-		Assert.assertEquals("Врешь, не уйдешь! Повтори ход", logic.answer("Елабуга", data));
+		logic.answer("пїЅпїЅпїЅпїЅпїЅ", data);
+		Assert.assertEquals("пїЅпїЅпїЅпїЅпїЅ, пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ! пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ", logic.answer("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ", data));
 	}
 	
 	@Test
 	void testComputerLose() {
 		var logic = new GameLogic();
-		Assert.assertEquals("Я проиграл", logic.answer("Гомель", new Data()));
+		Assert.assertEquals("пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", logic.answer("пїЅпїЅпїЅпїЅпїЅпїЅ", new Data()));
 	}
 }
