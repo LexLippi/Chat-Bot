7206510e399cda1cbe31047f4e701ecec9c1ba37
LexLippi <43090493+LexLippi@users.noreply.github.com>
2019-12-19 12:57:01 +0500

    add board data tests

--- src/test/java/board_game_test/TestDifferentBoardGameLevel.java
+++ src/test/java/board_game_test/TestDifferentBoardGameLevel.java
@@ -1,4 +1,28 @@
 package board_game_test;
 
+import chat_bot.game.board_game.board_levels.BoardLevel;
+import chat_bot.game.board_game.board_levels.Easy;
+import chat_bot.game.board_game.board_levels.Hard;
+import chat_bot.game.board_game.board_levels.Medium;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class TestDifferentBoardGameLevel {
+    @Test
+    public void testDifferentBoardGameLevel() {
+        var easyData = new Easy().sortData();
+        var mediumData = new Medium().sortData();
+        var hardData = new Hard().sortData();
+        for (var el: hardData.keySet()) {
+            Assert.assertFalse(easyData.containsKey(el));
+        }
+        for (var el: mediumData.keySet()) {
+            Assert.assertFalse(easyData.containsKey(el));
+        }
+        for (var el: mediumData.keySet()) {
+            Assert.assertFalse(hardData.containsKey(el));
+        }
+        Assert.assertEquals(easyData.size(), mediumData.size() + 1);
+        Assert.assertEquals(easyData.size(), hardData.size() + 1);
+    }
 }
